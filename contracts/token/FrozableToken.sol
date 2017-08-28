0d59409d5ecc6b9491b4f8daccbbf493cfbbf03d
Andres Elizondo <acesxx@gmail.com>
2017-08-28 13:46:05 +0200

    Using super._transfer() for Frozable Tokens

--- contracts/token/FrozableToken.sol
+++ contracts/token/FrozableToken.sol
@@ -17,13 +17,8 @@ contract Frozable is Token {
 
 	// Overrides the internal _transfer function with Frozable attributes
 	function _transfer(address _from, address _to, uint _value) internal {
-		require (_to != 0x0);								// Prevent transfer to 0x0 address. Use burn() instead
-		require (balanceOf[_from] >= _value);				// Check if the sender has enough
-		require (balanceOf[_to] + _value > balanceOf[_to]); // Check for overflows
 		require (!frozenAccount[_from]);					// Check that both accounts
 		require (!frozenAccount[_to]);						// are not currently frozen
-		balanceOf[_from] -= _value;							// Subtract from the sender
-		balanceOf[_to] += _value;							// Add the same to the recipient
-		Transfer(_from, _to, _value);						// Notify anyone listening that this transfer took place
+		super._transfer(_from, _to, _value);                // Performs the normal transfer from Basic Token
 	}
 }
